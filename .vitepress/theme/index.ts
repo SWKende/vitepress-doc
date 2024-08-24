2380888c5e7351d7ee78900b51f658ac8a94969a
SWKENDE <shanwenkang@maimikj.com>
2024-08-24 15:25:23 +0800

    新增图片缩放的功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,6 +7,10 @@ import DataPanel from "./components/DataPanel.vue";
 
 import Confetti from "./components/Confetti.vue";
 
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+
 
 export default {
     extends: DefaultTheme,
@@ -19,5 +23,19 @@ export default {
                 busuanzi.fetch();
             };
         }
+    },
+    setup() {
+        const route = useRoute();
+        const initZoom = () => {
+            // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+            mediumZoom(".main img", { background: "var(--vp-c-bg)" }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+        };
+        onMounted(() => {
+            initZoom();
+        });
+        watch(
+            () => route.path,
+            () => nextTick(() => initZoom())
+        );
     },
 }
